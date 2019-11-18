666534174595bc98474040809b1fc863c95f1422
SB604-14 <SB604-14@DESKTOP-P25BHNP>
2019-11-18 14:25:48 +0700

    Penambahan fungsi tampilTPemainInti

--- ContohVersioning/src/main/java/org/if_unpas/kpl/ContohVersioning/Tim.java
+++ ContohVersioning/src/main/java/org/if_unpas/kpl/ContohVersioning/Tim.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Tim {
 	private String nama;
-	private List<Pemain> pemaintInti = new ArrayList<Pemain>();
+	private List<Pemain> pemainInti = new ArrayList<Pemain>();
 	
 	public Tim() {
 		nama = "";
@@ -13,6 +13,13 @@ public class Tim {
 
 	public Tim(String nama) {
 		this.nama = nama;
+	}
+	
+	public void tampilPemainInti() {
+		for(int i =0; i<pemainInti.size();i++) {
+			Pemain p = pemainInti.get(i);
+			System.out.println(p.getNoPunggung() + " # " + p.getNama());
+		}
 	}
 
 }
